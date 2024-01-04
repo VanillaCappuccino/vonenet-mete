b1a21b0b2a5d558917f0854f9c68b628ceecc723
VanillaCappuccino <metehergul2002@gmail.com>
2024-01-05 00:55:03 +0300

    abc

--- imagenet_download.sh
+++ imagenet_download.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #shell script for full imagenet1k download.
 
-python3 imagenet_download.py --output_path /homes/mh2071/datasets/imagenet --torrent_path torrents
+python3 imagenet_download.py --output_path /datasets/imagenet --torrent_path torrents
 
-cd /homes/mh2071/datasets/imagenet
+cd /datasets/imagenet
 
-mv ILSVRC2012_img_train.tar /homes/mh2071/datasets/imagenet/train/ && cd /homes/mh2071/datasets/imagenet/train && tar -xvf ILSVRC2012_img_train.tar
+mv ILSVRC2012_img_train.tar /datasets/imagenet/train/ && cd /datasets/imagenet/train && tar -xvf ILSVRC2012_img_train.tar
 rm -f ILSVRC2012_img_train.tar
 find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
 
-cd /homes/mh2071/datasets/imagenet
+cd datasets/imagenet
 
-mv ILSVRC2012_img_val.tar /homes/mh2071/datasets/imagenet/val && cd /homes/mh2071/datasets/imagenet/val && tar -xvf ILSVRC2012_img_val.tar
+mv ILSVRC2012_img_val.tar /datasets/imagenet/val && cd /datasets/imagenet/val && tar -xvf ILSVRC2012_img_val.tar
 rm -f ILSVRC2012_img_test.tar
 wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
