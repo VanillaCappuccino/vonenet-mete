e4c8d3bb94c3d1f20fca68dae0199ed5f71478e4
VanillaCappuccino <metehergul2002@gmail.com>
2024-01-05 02:04:47 +0300

    test

--- imagenet_download.py
+++ imagenet_download.py
@@ -38,10 +38,10 @@ if not os.path.exists(FLAGS.output_path + "/test"):
     # print("Creating test path")
     os.mkdir(FLAGS.output_path + "/test")
 
-if not os.path.exists(f"{FLAGS.output_path}/ILSVRC2012_img_train.tar"):
+# if not os.path.exists(f"{FLAGS.output_path}/ILSVRC2012_img_train.tar"):
 
-    torrent_file = TorrentDownloader("torrents/train.torrent", f'{FLAGS.output_path}')
-    torrent_file.start_download()
+#     torrent_file = TorrentDownloader("torrents/train.torrent", f'{FLAGS.output_path}')
+#     torrent_file.start_download()
 
 if not os.path.exists(f"{FLAGS.output_path}/ILSVRC2012_img_val.tar"):
 
--- imagenet_download.sh
+++ imagenet_download.sh
@@ -9,7 +9,7 @@ mv ILSVRC2012_img_train.tar /datasets/imagenet/train/ && cd /datasets/imagenet/t
 rm -f ILSVRC2012_img_train.tar
 find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
 
-cd datasets/imagenet
+cd /datasets/imagenet
 
 mv ILSVRC2012_img_val.tar /datasets/imagenet/val && cd /datasets/imagenet/val && tar -xvf ILSVRC2012_img_val.tar
 rm -f ILSVRC2012_img_test.tar
