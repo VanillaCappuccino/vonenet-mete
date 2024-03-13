981d8b296c1aacec6bafcd6a206ef35b93ef05e2
VanillaCappuccino <metehergul2002@gmail.com>
2024-03-13 15:29:55 +0000

    ram aaaaaa

--- vonenet.sh
+++ vonenet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python prepare_dataset.py
-python train.py --in_path tiny-224 -o vonenet-run1 --model_type vonenetdn --ngpus 2 --workers 4 --epochs 5 train
+python train.py --in_path tiny-224 -o vonenet-run1 --model_type vonenetdn --ngpus 2 --workers 2 --epochs 5 train
