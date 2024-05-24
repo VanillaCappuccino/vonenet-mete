0cf1d99155beb3b176fd4388799dbb90abdc22e2
VanillaCappuccino <metehergul2002@gmail.com>
2024-05-24 17:44:19 +0100

    devices

--- vonenet/modules.py
+++ vonenet/modules.py
@@ -360,7 +360,7 @@ class GaussianDNBlock(nn.Module):
 
         # NEEDS TO HAPPEN ON CPU!!!
 
-        weights = torch.zeros(self.bank_size, self.bank_size, self.in_size, self.in_size, device=dvc)
+        weights = torch.zeros(self.bank_size, self.bank_size, self.in_size, self.in_size, device=device)
 
         # x = torch.linspace(-1, 1, self.in_size, device="cpu")
         # params = self.params.to("cpu")
