3c4c39c9987af96d19b8db84c9dfe25105205a50
VanillaCappuccino <metehergul2002@gmail.com>
2024-05-25 18:13:51 +0100

    debug

--- cov_matrix_plotter.sh
+++ cov_matrix_plotter.sh
@@ -1,3 +1,3 @@
 python cov_matrix_plotter.py --cov_path 32x32x512x2525xl2norm --trainable_vonenetdn \\
---ckpt checkpoints/vonenetdn-cov-variable-l2 \\
+--ckpt checkpoints/vonenetdn-cov-variable-l2/latest_epoch.pth.tar \\
 --images --gabors --rfs --denominators --normed --image_edge 3 --plots_edge 3
