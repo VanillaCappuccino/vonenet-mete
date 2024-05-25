018ab5d71f8d3ec355c58445c40d48f0322f2b6f
VanillaCappuccino <metehergul2002@gmail.com>
2024-05-25 17:42:54 +0100

    script adapt

--- cov_matrix_plotter.sh
+++ cov_matrix_plotter.sh
@@ -1,3 +1,4 @@
 python cov_matrix_plotter.py --cov_path 32x32x512x2525xl2norm \\
+--ckpt "checkpoints/vonenetdn-cov-variable-l2" \\
 --images --gabors --cov_matrix_rfs --denominators --normed \\
 --image_edge 3 --plots_edge 3
