8da53b7abd89bcfebdd253b0de88d79c15c1d1ac
VanillaCappuccino <metehergul2002@gmail.com>
2024-05-26 00:16:22 +0100

    ckpt

--- imagenet-p-rn18.sh
+++ imagenet-p-rn18.sh
@@ -4,7 +4,7 @@ function runAllPerturbations(){
    arr=("$@")
    for i in "${arr[@]}";
       do
-          python imagenet-p-eval.py -m resnet18 -p "$i" --ngpu 1 --num_workers 16 --rn18_checkpoint checkpoints//epoch_60.pth.tar --data_dir /home/mete/repos/robustness/ImageNet-P/datasets/Tiny-ImageNet-P
+          python imagenet-p-eval.py -m resnet18 -p "$i" --ngpu 1 --num_workers 16 --rn18_checkpoint checkpoints/rn18_non_pretrained_200/epoch_60.pth.tar --data_dir /home/mete/repos/robustness/ImageNet-P/datasets/Tiny-ImageNet-P
           #-cp sthsth
       done
 
