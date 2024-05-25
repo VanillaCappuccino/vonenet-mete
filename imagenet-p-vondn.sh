eaab413371608a391180750f894bf0cc3f63f52b
VanillaCappuccino <metehergul2002@gmail.com>
2024-05-25 21:25:36 +0100

    bash fix

--- imagenet-p-vondn.sh
+++ imagenet-p-vondn.sh
@@ -4,12 +4,12 @@ function runAllPerturbations(){
    arr=("$@")
    for i in "${arr[@]}";
       do
-          python imagenet-p-eval.py -m vonenet -p "$i" --ngpu 1 --num_workers 16 \\
-          --cov_path 32x32x512x2525xl2norm \\
-          --vonenetdn_checkpoint checkpoints/vonenetdn-cov-variable-l2/latest_epoch.pth.tar \\
-          --data_dir /home/mete/repos/robustness/ImageNet-P/datasets/Tiny-ImageNet-P \\
-          --trainable_vonenetdn \\
-          --sf_min 0.5 --sf_max 11.2 \\
+          python imagenet-p-eval.py -m vonenet -p "$i" --ngpu 1 --num_workers 16 \
+          --cov_path 32x32x512x2525xl2norm \
+          --vonenetdn_checkpoint checkpoints/vonenetdn-cov-variable-l2/latest_epoch.pth.tar \
+          --data_dir /home/mete/repos/robustness/ImageNet-P/datasets/Tiny-ImageNet-P \
+          --trainable_vonenetdn \
+          --sf_min 0.5 --sf_max 11.2 \
 
 
           #-cp sthsth
